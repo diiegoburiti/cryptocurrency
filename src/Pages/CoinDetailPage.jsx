d2c68854e470d3fc74e98998a1e6fe7c324d27e3
Diego Buriti <bs.diego97@gmail.com>
2020-08-28 18:35:20 -0300

    Fetch data for detail page.

--- src/Pages/CoinDetailPage.jsx
+++ src/Pages/CoinDetailPage.jsx
@@ -1,10 +1,54 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import HisotryChart from "../components/HistoryChart";
+import CoinData from "../components/CoinData";
+import coinGecko from "../api/coinGecko";
 
 const CoinDetailPage = () => {
   const { id } = useParams();
-  console.log(id);
-  return <div></div>;
+  const [coinDate, setCoinData] = React.useState({});
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const [day, week, year] = await Promise.all([
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: "usd",
+            days: "1",
+          },
+        }),
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: "usd",
+            days: "7",
+          },
+        }),
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: "usd",
+            days: "365",
+          },
+        }),
+      ]);
+
+      setCoinData({
+        day: day.data.prices,
+        weeek: week.data.prices,
+        year: year.data.prices,
+      });
+    }
+    fetchData();
+  }, [id]);
+
+  function renderData() {
+    return (
+      <div className="coinlist">
+        <HisotryChart />
+        <CoinData />
+      </div>
+    );
+  }
+  return renderData();
 };
 
 export default CoinDetailPage;
---
+++ src/components/HistoryChart.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HisotryChart = () => {
+  return <div></div>;
+};
+
+export default HisotryChart;
