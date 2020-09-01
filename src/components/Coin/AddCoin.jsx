28f822857a1db1d916583dc03edd0b292ce8f177
Diego Buriti <bs.diego97@gmail.com>
2020-09-01 20:39:46 -0300

    Added add icons compoenents and dropdown menu.

--- src/components/Coin/AddCoin.jsx
+++ src/components/Coin/AddCoin.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { WatchListContext } from "../../context/watchListContext";
 
 const AddCoin = () => {
-  return <div>Add coin</div>;
+  const [active, setActive] = useState(false);
+  const { addCoin } = useContext(WatchListContext);
+  const availableCoins = [
+    "bitcoin",
+    "ethereum",
+    "ripple",
+    "tether",
+    "bitcoin-cash",
+    "litecoin",
+    "eos",
+    "okb",
+    "tezos",
+    "cardano",
+  ];
+
+  function handleClick(coin) {
+    addCoin(coin);
+    setActive(false);
+  }
+
+  return (
+    <div className="dropdown">
+      <button
+        className="btn btn-primary dropdown-toggle"
+        type="button"
+        onClick={() => setActive(!active)}
+      >
+        Add Coin
+      </button>
+
+      <div className={active ? "dropdown-menu show" : "dropdown-menu"}>
+        {availableCoins.map((coin) => {
+          return (
+            <a
+              onClick={() => handleClick(coin)}
+              key={coin}
+              href="#"
+              className="dropdown-item"
+            >
+              {coin}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default AddCoin;
--- src/context/watchListContext.js
+++ src/context/watchListContext.js
@@ -5,16 +5,22 @@ export const WatchListContext = createContext();
 export const WatchListContextProvider = (props) => {
   const [watchList, setwatchList] = useState(["bitcoin", "ripple", "ethereum"]);
 
+  function addCoin(coin) {
+    if (watchList.indexOf(coin) === -1) {
+      setwatchList([...watchList, coin]);
+    }
+  }
+
   function deleteCoin(coin) {
     setwatchList(
       watchList.filter((el) => {
         return el !== coin;
-      })
+      }),
     );
   }
 
   return (
-    <WatchListContext.Provider value={{ watchList, deleteCoin }}>
+    <WatchListContext.Provider value={{ watchList, deleteCoin, addCoin }}>
       {props.children}
     </WatchListContext.Provider>
   );
