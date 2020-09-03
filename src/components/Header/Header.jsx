14a28a718348ba3e3e0408c27fc7cb7a23d2b207
Diego Buriti <bs.diego97@gmail.com>
2020-09-03 10:26:42 -0300

    Styled Header without bootstrap.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className={style.header}>CriptoCoins</h1>
+    </div>
+  );
+};
+
+export default Header;
